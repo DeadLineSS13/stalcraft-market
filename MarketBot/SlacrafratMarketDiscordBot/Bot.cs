680a9f638a70384e16203ab6bb9e442da3e7a31c
DeadLine <mishah8888@mail.ru>
2023-01-25 18:43:51 +0300

    First Commit

---
+++ MarketBot/SlacrafratMarketDiscordBot/Bot.cs
@@ -0,0 +1,54 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using DSharpPlus.CommandsNext;
+using System.Text;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+
+namespace SlacrafratMarketDiscordBot
+{
+    internal class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configJson = JsonConvert.DeserializeObject<Configuration>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug,
+                
+            };
+
+            Client = new DiscordClient(config);
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] {configJson.Prefix},
+                EnableDms = false,
+                EnableMentionPrefix = true
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(1);
+        }
+
+        private Task OnClientReady(ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
